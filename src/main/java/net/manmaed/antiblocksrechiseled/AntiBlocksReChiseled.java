def6053b47aa0073ea24f44bb88f2a4f711939e4
manmaed <manmaed18@gmail.com>
2023-02-05 13:12:56 +0000

    1.19.3 forge port??

--- src/main/java/net/manmaed/antiblocksrechiseled/AntiBlocksReChiseled.java
+++ src/main/java/net/manmaed/antiblocksrechiseled/AntiBlocksReChiseled.java
@@ -2,22 +2,20 @@ package net.manmaed.antiblocksrechiseled;
 
 import net.manmaed.antiblocksrechiseled.blocks.ABRCBrightColors;
 import net.manmaed.antiblocksrechiseled.blocks.ABRCWoolColors;
-import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(AntiBlocksReChiseled.MOD_ID)
 public class AntiBlocksReChiseled {
 
     public static final String MOD_ID = "antiblocksrechiseled";
-    public static final CreativeModeTab itemGroup = new CreativeModeTab(MOD_ID) {
-        @Override
-        public ItemStack makeIcon() {
-            return new ItemStack(ABRCBrightColors.BRIGHT_WHITE_BORDER.get());
-        }
-    };
 
     public AntiBlocksReChiseled() {
         IEventBus event = FMLJavaModLoadingContext.get().getModEventBus();
@@ -25,6 +23,22 @@ public class AntiBlocksReChiseled {
         ABRCBrightColors.BLOCKS.register(event);
         ABRCWoolColors.ITEMS.register(event);
         ABRCBrightColors.ITEMS.register(event);
+        event.addListener(this::createTab);
+    }
+
+    public void createTab(CreativeModeTabEvent.Register event) {
+        event.registerCreativeModeTab(new ResourceLocation(MOD_ID, "creative_tab"), builder -> builder
+                .icon(() -> new ItemStack(ABRCBrightColors.BRIGHT_WHITE_BORDER.get()))
+                .title(Component.translatable("itemGroup." + MOD_ID))
+                .displayItems((features, output, hasPrems) -> {
+                    for (Item item : ForgeRegistries.ITEMS.getValues()) {
+                        if (item.getCreatorModId(item.getDefaultInstance()).equals(MOD_ID)) {
+                            //LogHelper.warn("This item is form this mod" + item.toString());
+                            output.accept(item);
+                        }
+                    }
+                })
+        );
     }
 
 }
--- src/main/java/net/manmaed/antiblocksrechiseled/items/AnitBlockItem.java
+++ src/main/java/net/manmaed/antiblocksrechiseled/items/AnitBlockItem.java
@@ -1,11 +1,10 @@
 package net.manmaed.antiblocksrechiseled.items;
 
-import net.manmaed.antiblocksrechiseled.AntiBlocksReChiseled;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 
 public class AnitBlockItem extends BlockItem {
     public AnitBlockItem(Block block) {
-        super(block, new Properties().tab(AntiBlocksReChiseled.itemGroup));
+        super(block, new Properties());
     }
 }
