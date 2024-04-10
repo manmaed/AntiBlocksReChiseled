584a9c68392ed3fc015850f65f0b087276a8ba9f
manmaed <manmaed18@gmail.com>
2024-04-10 11:11:55 +0100

    Fix  Spelling

--- src/main/java/net/manmaed/antiblocksrechiseled/items/AntiBlockItem.java
+++ src/main/java/net/manmaed/antiblocksrechiseled/items/AntiBlockItem.java
@@ -4,8 +4,8 @@ import net.manmaed.antiblocksrechiseled.AntiBlocksReChiseled;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
 
-public class AnitBlockItem extends BlockItem {
-    public AnitBlockItem(Block block) {
+public class AntiBlockItem extends BlockItem {
+    public AntiBlockItem(Block block) {
         super(block, new Properties().tab(AntiBlocksReChiseled.itemGroup));
     }
 }
