147340b56ca098af838841a1918d061cb3fc498d
manmaed <manmaed18@gmail.com>
2024-04-24 00:16:55 +0100

    1.20.5

--- src/main/java/net/manmaed/antiblocksrechiseled/items/AntiBlockItem.java
+++ src/main/java/net/manmaed/antiblocksrechiseled/items/AntiBlockItem.java
@@ -1,11 +1,11 @@
 package net.manmaed.antiblocksrechiseled.items;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 
 public class AntiBlockItem extends BlockItem {
     public AntiBlockItem(Block block) {
-        super(block, new FabricItemSettings());
+        super(block, new Item.Settings());
     }
 }
