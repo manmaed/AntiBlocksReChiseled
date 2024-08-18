06ca6d29ca9b739405af59deda54af9824f147d6
manmaed <manmaed18@gmail.com>
2024-08-18 19:05:05 +0100

    add my own tags #12

---
+++ src/main/java/net/manmaed/antiblocksrechiseled/tag/ABRCTags.java
@@ -0,0 +1,21 @@
+package net.manmaed.antiblocksrechiseled.tag;
+
+import net.manmaed.antiblocksrechiseled.AntiBlocksReChiseled;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class ABRCTags {
+
+    public static final TagKey<Block> ANTIBLOCK_WITHOUT_BORDERS = create("antiblock_without_borders");
+    public static final TagKey<Block> ANTIBLOCK_WITH_BORDERS = create("antiblock_with_borders");
+
+    public static final TagKey<Block> ANTIBLOCK_BRIGHT_COLORS = create("antiblock_bright_colors");
+    public static final TagKey<Block> ANTIBLOCK_WOOL_COLORS= create("antiblock_wool_colors");
+
+
+    private static TagKey<Block> create(String name) {
+        return TagKey.create(BuiltInRegistries.BLOCK.key(), ResourceLocation.fromNamespaceAndPath(AntiBlocksReChiseled.MOD_ID, name));
+    }
+}
